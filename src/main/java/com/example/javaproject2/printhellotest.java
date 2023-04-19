111e0c41963d8f50b25bcce03216dac566cdfbeb
simhyeji <simhyeji70@gmail.com>
2023-04-19 11:18:25 +0900

    feat: first commit

---
+++ src/main/java/com/example/javaproject2/printhellotest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2;
+
+public class printhellotest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello(); // 인스턴스화
+        printHello.print();
+    }
+}
