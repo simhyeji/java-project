04a6af542341db3acb4819c2f94eec6e9000a49f
simhyeji <simhyeji70@gmail.com>
2023-04-19 14:47:12 +0900

    feat: 버퍼 두문장 받기

---
+++ src/main/java/week1/BufferedReaderExTest.java
@@ -0,0 +1,11 @@
+package week1;
+
+import java.io.IOException;
+public class BufferedReaderExTest {
+    public static void main(String[] args) throws IOException {
+// BufferedReaderEx 선언 및 초기화
+        BufferedReaderEx bufferedReaderEx = new BufferedReaderEx();
+// .readALine() 메소드 호출
+        bufferedReaderEx.readALine();
+    }
+}
